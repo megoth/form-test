6acea680644b1fa7445f28602d3034b798f24c3c
Arne Hassel <arne.hassel@gmail.com>
2025-12-30 23:41:21 +0100

    Adding tags to highlight optional and required fields

--- src/SimpleForm.tsx
+++ src/SimpleForm.tsx
@@ -1,10 +1,10 @@
 import {useForm, type SubmitHandler} from "react-hook-form"
-import {Button, ErrorSummary, Field, Input, Label, ValidationMessage} from "@digdir/designsystemet-react";
+import {Button, ErrorSummary, Field, Input, Label, Tag, ValidationMessage} from "@digdir/designsystemet-react";
 
 type Inputs = {
-    example: string
-    exampleRequired1: string
-    exampleRequired2: string
+    field1: string
+    field2: string
+    field3: string
 }
 
 export default function SimpleForm() {
@@ -17,7 +17,7 @@ export default function SimpleForm() {
     } = useForm<Inputs>()
     const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
 
-    console.log(watch("example"));
+    console.log(watch("field1"));
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} onReset={() => reset()}>
@@ -27,25 +27,34 @@ export default function SimpleForm() {
                     <ErrorSummary.List>
                         {Object.keys(errors).map((key) => (
                             <ErrorSummary.Item key={key}>
-                                <ErrorSummary.Link href={`#${key}`}>Field is required</ErrorSummary.Link>
+                                <ErrorSummary.Link href={`#${key}`}>Required field is missing value</ErrorSummary.Link>
                             </ErrorSummary.Item>
                         ))}
                     </ErrorSummary.List>
                 </ErrorSummary>
             )}
             <Field>
-                <Label>Not required field</Label>
-                <Input defaultValue="test" {...register("example")} />
+                <Label>
+                    <span>Field 1</span>
+                    <Tag data-color="info">Optional</Tag>
+                </Label>
+                <Input {...register("field1")} />
             </Field>
             <Field>
-                <Label>Required field 1</Label>
-                <Input id="exampleRequired1" {...register("exampleRequired1", {required: true})} />
-                {errors.exampleRequired1 && <ValidationMessage>This field is required</ValidationMessage>}
+                <Label>
+                    <span>Field 2</span>
+                    <Tag data-color="warning">Required</Tag>
+                </Label>
+                <Input id="field2" {...register("field2", {required: true})} />
+                {errors.field2 && <ValidationMessage>Missing value</ValidationMessage>}
             </Field>
             <Field>
-                <Label>Required field 2</Label>
-                <Input id="exampleRequired2" {...register("exampleRequired2", {required: true})} />
-                {errors.exampleRequired2 && <ValidationMessage>This field is required</ValidationMessage>}
+                <Label>
+                    <span>Field 3</span>
+                    <Tag data-color="warning">Required</Tag>
+                </Label>
+                <Input id="field3" {...register("field3", {required: true})} />
+                {errors.field3 && <ValidationMessage>Missing value</ValidationMessage>}
             </Field>
             <div className="form-controls">
                 <Button type="submit">Submit</Button>
